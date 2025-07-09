8f26808ba05946d81b36ddb0c81b8c5f64b9ac65
PedroBertha <phlikoskibertha@gmail.com>
2025-07-09 15:36:29 -0300

    adicionado lista para armazenar dados do formularaio
    index.css alterado

--- form/src/App.tsx
+++ form/src/App.tsx
@@ -1,5 +1,7 @@
 import { useForm, type SubmitHandler } from 'react-hook-form'
+import { useState } from 'react'
 import './App.css'
+
 
 interface FormValues {
   nome: string
@@ -12,54 +14,83 @@ export default function MeuFormulario() {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm<FormValues>()
 
+  const [submissoes, setSubmissoes] = useState<FormValues[]>([])
+
   const onSubmit: SubmitHandler<FormValues> = (data) => {
-    console.log("Dados validados:", data)
+    setSubmissoes((submissoes) => [...submissoes, data])
+    reset()
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label>Nome:</label>
-        <input
-          {...register("nome", {
-            required: "O nome é obrigátorio",
-            maxLength: { value: 50, message: "Máximo 50 caracteres" },
-          })}
-        />
-        {errors.nome && <span style={{ color: "red"}}>{errors.nome.message}</span>}
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>Nome:</label>
+          <input
+            {...register("nome", {
+              required: "O nome é obrigátorio",
+              maxLength: { value: 50, message: "Máximo 50 caracteres" },
+            })}
+          />
+          <br />
+          {errors.nome && <span style={{ color: "#646cff" }}>{errors.nome.message}</span>}
         </div>
-      
-      <div>
-        <label>Email:</label>
-        <input
-          type="email"
-          {...register("email", {
-            required: "Oemail é obrigátorio",
-            pattern: {
-              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-              message: "Formato de email Inválido",
-            },
-          })}
-        />
-        {errors.email && <span style={{ color: "red" }}>{errors.email.message}</span>}
-      </div>
-          
-      <div>
-        <label>Idade:</label>
-        <input
+
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            {...register("email", {
+              required: "O email é obrigátorio",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Formato de email Inválido",
+              },
+            })}
+          />
+          <br />
+          {errors.email && <span style={{ color: "#646cff" }}>{errors.email.message}</span>}
+        </div>
+
+        <div>
+          <label>Idade:</label>
+          <input
             type="number"
             {...register("idade", {
               required: "Informe a idade",
               min: { value: 10, message: "você precisa ter pelo menos 10 anos" },
               max: { value: 100, message: "Muito velho" },
-             })}
-            />
-            {errors.idade && <span style={{ color: "red" }}>{errors.idade.message}</span>}
-      </div>
+            })}
+          />
+          <br />
+          {errors.idade && <span style={{ color: "#646cff" }}>{errors.idade.message}</span>}
+        </div>
 
-      <button type="submit">Enviar</button>
-    </form>
+        <button type="submit">Enviar</button>
+      </form>
+
+      <h2>Lista de Envios</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Idade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {submissoes.map((item, idx) => (
+            <tr key={idx}>
+              <td>{item.nome}</td>
+              <td>{item.email}</td>
+              <td>{item.idade}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   )
 }
